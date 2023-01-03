798052caafe3244fc2f60cc1891aafb1bf52e7e1
giarchive <110273310+giarchive@users.noreply.github.com>
2023-01-03 22:28:20 +0900

    Add files via upload

--- script.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-        $("#head").load("/test/parts/head.html");
-           $("#header").load("/test/parts/header.html");
-           $("#fooder").load("/test/parts/fooder.html");
-});
