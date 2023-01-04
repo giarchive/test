00fb79f492f0c40c36c2b41df82a28ba4c820130
giarchive <110273310+giarchive@users.noreply.github.com>
2023-01-04 17:18:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-fetch("header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector(".header").innerHTML = data;
-});
-fetch("head.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector(".head").innerHTML = data;
-});
-fetch("footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector(".footer").innerHTML = data;
-});
+$(document).ready(function(){
+$("#head").load("/parts/head.html");
+$("#header").load("/parts/header.html");
+$("#footer").load("/parts/footer.html");
 });
